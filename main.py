68f0e66fb6ededc41166a01cbb8f218b449f17a2
kbgreenberg72 <kyle.greenberg93@gmail.com>
2024-04-12 11:52:34 -0400

    Corrected some bugs with how I took in p aramaeters in the url as I was using {} instead of <> which is used in flask. Also changed Post to POST and added Json data to the test request.

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/Post/v2/companies/{companyID}/employees", methods=['Post'])
-def add_new_employee():
+@app.route("/Post/v2/companies/<companyID>/employees", methods=['POST'])
+def add_new_employee(companyID):
     data = request.get_json()
     print(data)
     return "ok", 200
 
-@app.route("/Post/v2/companies/{companyID}/employees/{employeeId}", methods=['Post'])
-def update_employee():
+@app.route("/Post/v2/companies/<companyID>/employees/<employeeId>", methods=['Post'])
+def update_employee(companyID, employeeId):
     data = request.get_json()
     print(data)
     return "ok", 200
 
 
 def main():
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
     
     
 
--- test_request.py
+++ test_request.py
@@ -1,7 +1,13 @@
 import requests
 
+data = {
+    'name': 'John Doe',
+    'position': 'Software Engineer'
+}
+
 url = "http://127.0.0.1:5000/Post/v2/companies/1/employees"
 
-response = requests.post(url)
+response = requests.post(url, json=data)
 
-print(response.text)
+print(f"Status Code: {response.status_code}")
+print(f"Response Test: {response.text}")
